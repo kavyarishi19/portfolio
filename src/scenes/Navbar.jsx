38e057260973ea462f6ed0f4f4ce707e4882bb8e
kavyarishi19 <forcodingpurposes19@gmail.com>
2023-03-26 01:11:24 +0530

    Navbar and Navigation

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -16,22 +16,94 @@ const Link = ({page, selectedPage, setSelectedPage}) =>{
     )
 }
 
-const Navbar = ({selectedPage, setSelectedPage}) => {
+const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-red";
 
     return (
-        <nav className={`z-40 w-full fixec top-0 py-6`}>
+        <nav className={`${navbarBackground} z-40 w-full fixec top-0 py-6`}>
             <div className="flex items-center justify-between mx-auto w-5/6">
                 <h4 className="font-playfair text-3xl font-bold">Rishhh</h4>
 
                 {/* DESKTOP NAV */}
                 {isAboveSmallScreens ? (
                     <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
-
+                        <Link
+                            page="Home"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Skills"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Projects"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Testmonials"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Contact"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
                     </div>
-                ) : (<div></div>)}
+                ) : (<button className="rounded-full bg-red p-2"
+                        onClick={() => setIsMenuToggled(!isMenuToggled)}
+                    >
+                     <img alt="menu-icon" src="./assets/menu-icon.svg"/>
+                    </button>
+                )}
+
+                {!isAboveSmallScreens && isMenuToggled &&(
+                    <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+                {/* MOBILE MENU POPUP */}
+                        {/* CLOSE ICON */}
+                        <div className="flex justify-end p-12">
+                            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                                <img alt="close-icon" src="./assets/close-icon.svg"></img>
+                            </button>
+                        </div>
+
+                        {/* MENU ITEMS */}
+                        <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+                        <Link
+                            page="Home"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Skills"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Projects"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Testmonials"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Contact"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        </div>
+                    </div>
+                )}
             </div>
         </nav>
     )
 }
+export default Navbar;
